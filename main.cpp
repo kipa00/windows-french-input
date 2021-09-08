b200588b6a873e6d082bdf77a42ad65548ffac0d
Yeonghyeon Kim <k2pa00@gmail.com>
2021-09-08 21:45:57 +0900

    use hooks to cancel keyboard input

--- main.cpp
+++ main.cpp
@@ -78,30 +78,48 @@ inline void substitute(int &v, const int value, int &no) {
     }
 }
 
-int main() {
-    int last, cnt = 0, no = 0;
-    while (1) {
-        for (int i=0; i<255; ++i) {
-            if (GetAsyncKeyState(i) & 1) {
-                if (i != 16 && i != 164 && i != 165 && i != 18 && !('a' <= i && i <= 'i') && i != 186) {
-                    if (!no) cnt = 0;
-                    if ((GetAsyncKeyState(16) & 0x8000) && 'A' <= i && i <= 'Z') {
-                        substitute(last, i | 256, no);
-                    } else if (last == 'O' && i == 'E') {
-                        substitute(last, 512, no);
-                    } else {
-                        substitute(last, i, no);
-                    }
-                } else if (i == 186 && precheck(last)) {
-                    ++no;
-                    remove_one();
-                    if (++cnt >= 2) {
-                        Sleep(1);
-                        no += process(last, cnt - 2);
-                    }
+int no, cnt, last;
+
+LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam) {
+    if (nCode == HC_ACTION) {
+        if (wParam == WM_KEYDOWN) {
+            PKBDLLHOOKSTRUCT p = (PKBDLLHOOKSTRUCT)lParam;
+            int i = p->vkCode;
+            if (i != 16 && i != 164 && i != 165 && i != 18 && !('a' <= i && i <= 'i') && i != 186) {
+                if (!no) cnt = 0;
+                if ((GetAsyncKeyState(16) & 0x8000) && 'A' <= i && i <= 'Z') {
+                    substitute(last, i | 256, no);
+                } else if (last == 'O' && i == 'E') {
+                    substitute(last, 512, no);
+                } else {
+                    substitute(last, i, no);
                 }
+            } else if (i == 186 && precheck(last)) {
+                if (++cnt >= 2) {
+                    ++no;
+                    Sleep(1);
+                    no += process(last, cnt - 2);
+                }
+                return 1;
             }
         }
     }
+    return CallNextHookEx(NULL, nCode, wParam, lParam);
+}
+
+int main() {
+    HHOOK hhkLowLevelKybd = SetWindowsHookEx(WH_KEYBOARD_LL, LowLevelKeyboardProc, 0, 0);
+    if (!hhkLowLevelKybd) {
+        printf("Failed to Create Hook");
+        return 0;
+    }
+    MSG msg;
+
+    while (!GetMessage(&msg, NULL, NULL, NULL)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    UnhookWindowsHookEx(hhkLowLevelKybd);
     return 0;
 }
